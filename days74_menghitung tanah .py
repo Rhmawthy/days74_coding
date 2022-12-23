8d70037d3f751b43cb76382346b561419f9f22e1
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-12-23 18:55:02 +0700

    Add files via upload

---
+++ days74_menghitung tanah .py
@@ -0,0 +1,10 @@
+'''program menhitung luas tanah dan
+menentukan harga tanah. ketika harga tanah permeter 100.000'''
+
+
+p = int(input("panjang tanah : "))
+l = int(input("luas tanah : "))
+
+rumus = ( p*l ) / 100000
+
+print (rumus)
